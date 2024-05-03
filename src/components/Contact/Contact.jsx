96f214cd56e85edda9df31f9f756fb01cff4019d
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-05-03 10:31:38 +0300

    end2

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import css from "./Contact.module.css";
 import { deleteContact } from "../../redux/contactsSlice";
 
-export default function ContactList({ name, number, id }) {
+export default function Contact({ name, number, id }) {
   const dispatch = useDispatch();
   return (
     <div className={css.cont}>
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,7 +2,7 @@ import css from "../SearchBox/SearchBox.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { filterContacts } from "../../redux/filtersSlice";
 
-export default function Filter() {
+export default function SearchBox() {
   const dispatch = useDispatch();
   const filterVal = useSelector((state) => state.filter.value);
 
